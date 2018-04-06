144ce615af1bdb17145d0b068f4a8c2a2df9afa5
gregoriorenz <32734428+gregoriorenz@users.noreply.github.com>
2018-04-06 21:41:03 +0800

    added php temp

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+
+?>
+
+<html>
+<head>
+</head>
+
+<body>   
+<form action = "post_comment.php" method = "POST">
+Comments: <textarea rows = "10" cols = "30" name = "commentContent"> </textarea> <br/>
+Name: <input type = "text" name = "name"><br/>
+<input type = "submit" value= "Post!"> <br/>
+
+</form>
+</body>
+</html>
